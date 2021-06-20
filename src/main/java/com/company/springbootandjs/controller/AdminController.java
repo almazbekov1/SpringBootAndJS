c174f8cb4ed647a9be62ad708596aabe05f207ae
Aidar <03almabek0v@gmail.com>
2021-06-20 15:18:29 +0600

    delete

--- src/main/java/com/company/springbootandjs/controller/AdminController.java
+++ src/main/java/com/company/springbootandjs/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -28,16 +29,38 @@ public class AdminController {
     @CrossOrigin
     @PostMapping
     public User addUser(@RequestBody User user){
+        System.out.println("post mapping");
+        System.out.println();
         return userRepo.save(user);
     }
     @CrossOrigin
     @GetMapping("/{id}")
     public User getUserById(@PathVariable("id") int id){
+        System.out.println("get id mapping");
+        System.out.println();
+
         return userRepo.getById((long)id);
     }
     @CrossOrigin
     @PutMapping
     public User putUser(@RequestBody User user){
+        System.out.println("put mapping");
+        System.out.println(user.getLastName());
+        System.out.println(user.getFirstName());
+        System.out.println(user.getEmail());
+        System.out.println();
+
+        return userRepo.save(user);
+    }
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public User putUserId(@PathVariable("id") int id,@RequestBody User user){
+        System.out.println("put mapping");
+        System.out.println("put id mapping");
+        System.out.println();
+        System.out.println();
+
+        user.setId((long)id);
         return userRepo.save(user);
     }
 
